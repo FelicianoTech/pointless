6f31651f208ea8acdf1148437ec45f4b91256c54
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-05-04 19:45:13 -0400

    Fix follow button alignment. (#52)

--- github.js
+++ github.js
@@ -3,7 +3,7 @@ function addRepoPageItems(){
 	if( $( "div.repohead h1 a.cci" ).length == 0 ){
 	
 		// Start with a grey status indicator until we determine the project's build status
-		$("div.repohead h1 strong[itemprop='name']").after('<a class="cci" href="" title="This project is not built on CircleCI"><span class="default-build-status"></span></a>');
+		$("div.repohead h1 strong[itemprop='name']").after('<a class="cci status-dot" href="" title="This project is not built on CircleCI"><span class="default-build-status"></span></a>');
 	}
 
 	if( $( ".cci.btn" ).length == 0 ){
